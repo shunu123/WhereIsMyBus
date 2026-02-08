cfd33433597e3250f5aaa1683221993c33ac7660
shunu <shunushunu91@gmail.com>
2026-02-08 15:30:19 +0530

    Initial Commit

---
+++ WhereIsMyBus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhereIsMyBus
+//
+//  Created by bujjuuu on 08/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhereIsMyBus/WhereIsMyBusApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhereIsMyBusApp.swift
+//  WhereIsMyBus
+//
+//  Created by bujjuuu on 08/02/26.
+//
+
+import SwiftUI
+
+@main
+struct WhereIsMyBusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
